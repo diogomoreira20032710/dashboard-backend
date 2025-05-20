ae8eaf62d6a72ed256f2d1a059b5cb34167a76e4
Diogo Moreira <diogo.moreira@cm-paredes.pt>
2025-05-20 13:06:52 +0100

    Ativar SSL na ligação PostgreSQL para Render

--- models/db.js
+++ models/db.js
@@ -2,11 +2,14 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'inventario',
-  password: process.env.DB_PASSWORD || '1234',
-  port: Number(process.env.DB_PORT || 5432), // Aqui estava 3001 → corrigido para 5432
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+  ssl: {
+    rejectUnauthorized: false // Importante para a Render aceitar a ligação
+  }
 });
 
 pool.connect()
